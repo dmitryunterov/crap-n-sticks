b1c4eb3782096befe3ae8e8242dcb562fceef540
dmitryunterov <DMIUNT@live.spbstu.ru>
2015-11-11 19:33:59 +0300

    начал вникать в whoami

---
+++ whoami/whoami.c
@@ -0,0 +1,10 @@
+#include <pwd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+//"шапку" взял из примера в мане по getpwnam. На всякий случай полностью.)
+int main(int argc, char *argv[])
+{
+
+}
