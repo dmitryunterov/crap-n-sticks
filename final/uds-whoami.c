9a320b9aaade05ec256dff7968e55e6a02f7dbf6
dmitryunterov <DMIUNT@live.spbstu.ru>
2015-11-12 10:24:09 +0300

    Рабочие программы и makefile созданный руками

---
+++ final/uds-whoami.c
@@ -0,0 +1,15 @@
+#include <pwd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+//"шапку" взял из примера в мане по getpwnam. На всякий случай полностью.)
+int main(int argc, char *argv[])
+{
+struct passwd *p;
+uid_t uid;
+uid = geteuid ();
+p = getpwuid (uid);
+puts(p->pw_name);
+return(0);
+}
