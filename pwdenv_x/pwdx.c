e64917fd433863b4a4c60c239116a02b48f41f25
dmitryunterov <DMIUNT@live.spbstu.ru>
2015-11-22 10:40:32 +0300

    не пашет но хоть компилится без ругани

--- pwdenv_x/pwdx.c
+++ pwdenv_x/pwdx.c
@@ -3,13 +3,14 @@
 //из мана по readlink
 #include <errno.h>
 
-int main(int argc, char const *argv[])
+int main(int argc, char  *argv[])
 {
 	char buf[1024];
-	int buffsize = sizeof(buf);
+	char* gde = argv[1];
+//Вероятно так надо указать указатель (мда).
 //про sizeof подсмотрел как сделано у коллег
-	readlink("/proc/argv[1]/cwd", buf, sizeof(buffsize));
-//явно указан путь, мне б хоть попробовать
-	printf("%s", buf);
+	int func = readlink(gde, buf, sizeof (buf));
+	buf[func] = '\0';
+	printf("%s\n", buf);
 	return 0;
 }
