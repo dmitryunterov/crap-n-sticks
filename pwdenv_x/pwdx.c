8457d1604298865973d2af55f8cc1e1e4fec9e53
dmitryunterov <DMIUNT@live.spbstu.ru>
2015-11-22 10:25:58 +0300

    Тупо врезал в путь argv[1]

--- pwdenv_x/pwdx.c
+++ pwdenv_x/pwdx.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <unistd.h>
 //из мана по readlink
+#include <errno.h>
 
 int main(int argc, char const *argv[])
 {
 	char buf[1024];
 	int buffsize = sizeof(buf);
 //про sizeof подсмотрел как сделано у коллег
-	readlink("/proc/1/cwd", buf, buffsize);
+	readlink("/proc/argv[1]/cwd", buf, sizeof(buffsize));
 //явно указан путь, мне б хоть попробовать
 	printf("%s", buf);
 	return 0;
