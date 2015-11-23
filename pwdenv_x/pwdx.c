71bb4415d2d3328a37968d7555406bd8e75af037
dmitryunterov <DMIUNT@live.spbstu.ru>
2015-11-23 12:55:45 +0300

    Мучения с подстановкой аргумента

--- pwdenv_x/pwdx.c
+++ pwdenv_x/pwdx.c
@@ -3,13 +3,16 @@
 //из мана по readlink
 #include <errno.h>
 
-int main(int argc, char  *argv[])
+int main(int argc, char* argv[])
 {
-	char buf[1024];
-	char* gde = argv[1];
+	char buf[256];
+	char* what=argv[1];
+	char* proc="proc";
+	char* siml="cwd";
+	char* gde =(/%s/%s/%s, proc, what, siml);
 //Вероятно так надо указать указатель (мда).
 //про sizeof подсмотрел как сделано у коллег
-	int func = readlink(gde, buf, sizeof (buf));
+	int func = readlink (gde, buf, sizeof (buf));
 	buf[func] = '\0';
 	printf("%s\n", buf);
 	return 0;
