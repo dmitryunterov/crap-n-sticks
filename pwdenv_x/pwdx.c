7dcd0e53507d9805cac94d99c5597fef5ddcf5a2
dmitryunterov <DMIUNT@live.spbstu.ru>
2015-11-23 14:02:02 +0300

    хоть немного работающий pwdx УРА

--- pwdenv_x/pwdx.c
+++ pwdenv_x/pwdx.c
@@ -1,17 +1,22 @@
 #include <stdio.h>
 #include <unistd.h>
-//из мана по readlink
 #include <errno.h>
+#include <string.h>
+//Спасибо Казарину, подсказал про strcat
 
 int main(int argc, char* argv[])
 {
-	char buf[256];
-	char* what=argv[1];
-	char* proc="proc";
-	char* siml="cwd";
-	char* gde =(/%s/%s/%s, proc, what, siml);
-//Вероятно так надо указать указатель (мда).
-//про sizeof подсмотрел как сделано у коллег
+	char gdeproc[256] = "/proc/";
+//Переменные, из значений которых склеится путь к cwd
+	char* argument1 = argv[1];
+//Аргументом 1 передаётся PID
+	char* skleika = strcat(strcat(gdeproc,argument1),"/cwd");
+//Вот эту магию понял так: функция strcat склеивает значение двух переменных 
+//gdeproc и argument1 а вышестоящий strcat склеивает это значение с /cwd и отдаёт
+//суммарное значение как значение пересенной skleika
+	char buf[1024];
+	char* gde = skleika;
+//Нужна ли эта переменная или readlink сожрёт сразу значение склейки?.
 	int func = readlink (gde, buf, sizeof (buf));
 	buf[func] = '\0';
 	printf("%s\n", buf);
