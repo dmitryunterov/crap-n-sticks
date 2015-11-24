5f160ecb0d18cbd0b94cd9fc9bbe566eece8ca54
dmitryunterov <DMIUNT@live.spbstu.ru>
2015-11-24 13:28:45 +0300

    добавил работу с аргументами

--- lab2/envx/envx.c
+++ lab2/envx/envx.c
@@ -1,11 +1,16 @@
 //Самая первая попытка сделать выводилку на экран текста из точно указанного файла
-#include<stdio.h>
-int main()
+#include <stdio.h>
+#include <string.h>
+int main(int argc, char* argv[])
 {
+	char gdeproc[256] = "/proc/";
+	char* argument1 = argv[1];
+	char* skleika = strcat(strcat(gdeproc,argument1),"/environ");
+
 	FILE *ptr_file;
 	char buf[8];
 
-   	ptr_file =fopen("/proc/2131/environ","r");
+   	ptr_file =fopen(skleika,"r");
     	if (!ptr_file)
       	return 1;
 
