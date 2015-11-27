9f5b766a40c729726ec1e4db37c40bb3aec998b7
dmitryunterov <DMIUNT@live.spbstu.ru>
2015-11-27 09:44:49 +0300

    поправленный envx

--- lab2/envx/envx.c
+++ lab2/envx/envx.c
@@ -6,6 +6,8 @@ int main(int argc, char* argv[])
 	char gdeproc[256] = "/proc/";
 	char* argument1 = argv[1];
 	char* skleika = strcat(strcat(gdeproc,argument1),"/environ");
+/*
+	Свои потуги закомментил, воспользовался рабочим рецептом
 
 	FILE *ptr_file;
 	char buf[8];
@@ -15,9 +17,24 @@ int main(int argc, char* argv[])
       	return 1;
 
     	while (fgets(buf,8, ptr_file)!=NULL)
+
 //	while (fgets(ptr_file)!=NULL)
         	printf("%s",buf);
 
 	fclose(ptr_file);
     	return 0;
+*/
+	FILE *ptr_file;
+	ptr_file = fopen(skleika, "r");
+	char c;
+	do
+{
+	c = fgetc(ptr_file);
+	if ( c == '\0' )
+	{
+	c = '\n';
+	}
+	printf("%c", c);
+}
+	while ( c != EOF );
 }
