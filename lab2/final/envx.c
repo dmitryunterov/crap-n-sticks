61e1a5eeaff49753eb5c7c8407ef4ccc21c2de52
dmitryunterov <DMIUNT@live.spbstu.ru>
2015-11-27 11:21:21 +0300

    поправил комменты, ридми и мэйкфайл

--- lab2/final/envx.c
+++ lab2/final/envx.c
@@ -1,15 +1,15 @@
-//Самая первая попытка сделать выводилку на экран текста из точно указанного файла
 #include <stdio.h>
 #include <string.h>
 #include "libmylib.h"
 
 int main(int argc, char* argv[])
 {
-//	char gdeproc[256] = "/proc/";
-//	char* argument1 = argv[1];
-//	char* skleika = strcat(strcat(gdeproc,argument1),"/environ");
 /*
-	Свои потуги закомментил, воспользовался рабочим рецептом
+	char gdeproc[256] = "/proc/";
+	char* argument1 = argv[1];
+	char* skleika = strcat(strcat(gdeproc,argument1),"/environ");
+Свои потуги закомментил, воспользовался рабочим рецептом
+//Самая первая попытка сделать выводилку на экран текста из точно указанного файла
 
 	FILE *ptr_file;
 	char buf[8];
@@ -19,15 +19,12 @@ int main(int argc, char* argv[])
       	return 1;
 
     	while (fgets(buf,8, ptr_file)!=NULL)
-
-//	while (fgets(ptr_file)!=NULL)
         	printf("%s",buf);
 
 	fclose(ptr_file);
     	return 0;
 
-	Всё что ниже - ушло в libmylib
-
+//Всё что ниже - ушло в libmylib
 
 	FILE *ptr_file;
 	ptr_file = fopen(skleika, "r");
